336887d4a3ed20530c7eaec1cbc6312ca0d6b00d
Sam White <samual.white13@imperial.ac.uk>
2014-07-04 17:41:47 +0100

    Implement more efficient method to find pareto-points
    - Add comments to core.clj

--- src/sym_regression_project/core.clj
+++ src/sym_regression_project/core.clj
@@ -5,8 +5,6 @@
 ;; This program will run a simple symbolic regression using data
 ;; supplied in interface.clj. 
 
-
-
 ;; Define the protected division function.
 (defn pdiv [x y] (if (zero? y) 1 (/ x y)))
 
@@ -39,17 +37,6 @@
   [n max-depth]
   (repeatedly n #(random-full-tree functions terminals (+ 1 (rand-int (- max-depth 1))))))
 
-;; First parameter to be optimised. Currently this performs a modified chi-squared test on the data.
-(defn score-1
-  [data ex]
-  (let [f (functionalise ex)]
-    (* -1 (apply + (map #(Math/abs (- (f (first %)) (second %))) data)))))
-
-;; Second parameter to be optimised. Currently this counts the number of nodes in an s-expression.
-(defn score-2
-[count-nodes ex]
-(* -1 (count-nodes ex)))
-
 ;; Generate zipper constructor.
 (defn expr-zip
   [expr]
@@ -73,6 +60,23 @@
   	(+ 1 (apply + (map count-nodes (rest ex))))
     1))
 
+;; First parameter to be optimised.
+(defn score-1
+  "This function calculates how well a candidate expression fits a supplied data set. This is
+   calculated using a modified chi squared test. A score of zero refers to an expression
+   that perfectly describes the data set. A score much below zero refers to an expression
+   that poorly descrives the data set."
+  [data ex]
+  (let [f (functionalise ex)]
+    (* -1 (apply + (map #(Math/abs (- (f (first %)) (second %))) data)))))
+
+;; Second parameter to be optimised.
+(defn score-2
+  "This function counts the number of nodes in an expression and outputs the negative of
+  that number. This is the metric by which an the complexity of an expression is determined."
+  [count-nodes ex]
+  (* -1 (count-nodes ex)))
+
 ;; Define mutation operation.
 (defn mutate-expr
   [expr new-tree-func]
@@ -95,7 +99,7 @@
         subtree2 (sub-tree expr2 target2)]
     [(tree-replace expr1 target1 subtree2) (tree-replace expr2 target2 subtree1)]))
 
-;; Implement tournament selection.
+;; Map fitness values onto an expression.
 (defn score-population
   [population score-1-func score-2-func]
   (map (fn [expr] {:expr expr 
@@ -105,23 +109,36 @@
                    :fitness 0})
        population))
 
-;; determine if one expression is dominated by another expression
+;; Determine if one expression is dominated by another expression. 
 (defn is-dominated
+  "This function tests whether expression x is dominated by function y.
+   An expression is dominated if both score-1 and score-2 of a comparison
+   expression is better. "
   [x y]
   (if (or (and (<= (:score-1 x) (:score-1 y)) (< (:score-2 x) (:score-2 y))) 
           (and (< (:score-1 x) (:score-1 y)) (<= (:score-2 x) (:score-2 y))))
     (assoc x :dominated true) x))
 
+;; Determine whether a function should go into the archive.
 (defn should-be-archived
+  "If expression x is dominated by expression y and empty list is
+   outputted. If expression x is not dominated, x is outputted."
   [is-dominated x y]
   (if (:dominated (reduce is-dominated x y))
     () x))
 
 (defn into-archive
+  "This function maps _should-be-archived_ over every value in
+   sequence x. The function outputs a sequence of all expression of x
+   that should go into the archive."
   [should-be-archived is-dominated x y]
   (map #(should-be-archived is-dominated % y) x))
 
+;; In the SPEA algorithm, fitness is determined by a strength function.
 (defn strength-func
+  "The strength of an individual in the archive is determined by the number
+   of current population members it dominates divided by (1 + population size).
+   An archive member's fitness is equal to its strength."
   [archive popn]
   (assoc archive :fitness 
                   (/ 
@@ -129,18 +146,24 @@
                     (filter #(and (>= (:score-1 archive) (:score-1 %)) (>= (:score-2 archive) (:score-2 %))) popn))
                     (+ 1 (count popn)))))
 
-;; sum strengths of archive members
 (defn sum-strengths
+  "This function sums the strengths of all supplied archive members."
   [archive]
   (reduce + (map #(:fitness %) archive)))
 
 (defn fitness-population
+  "The fitness of a population individual is determined by 1 + the sum of strengths
+   of all the individuals that cover it."
   [popn scored-archive]
   (assoc popn 
     :fitness (+ 1 
               (sum-strengths (filter #(and (<= (:score-1 popn) (:score-1 %)) (<= (:score-2 popn) (:score-2 %))) scored-archive)))))
 
+;; Define tournament selection function. 
 (defn tournament-selector
+  "This function selects a number of individuals from a population determined by
+   tournament-size. The tournament candidate with the minimum fitness value is
+   outputted."
   [scored-popn tournament-size]
   (let [competitors (repeatedly tournament-size #(rand-nth scored-popn))]
     (:expr (apply min-key :fitness competitors))))
